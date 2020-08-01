96969b0e43d94971eab17583df73c8bd54abf0b0
Mikhail <epictapok@gmail.com>
2020-08-01 16:57:54 +0300

    first version app courseJS

---
+++ lesson__12.js
@@ -0,0 +1,50 @@
+'use strict';
+// Задание на урок
+
+// 1) Создать переменную numberOfFilms и в нее поместить ответ пользовател на впорос
+// "сколько фильмов вы уже посмотрели?"
+// 2) Создать обьект personaMovieDB и в него поместить такие свойства:
+//  - count - сюда передается ответ на первый вопрос
+//  - movies - в это свойство поместить пустой обьект
+//  - actors - тоже поместить пустой обьект
+//  - genres - сюда поместить пустой массив
+//  - private - в это свойство поместить boolean значение false
+//
+// 3) Задайте пользователю по два раза вопросы:
+// - 'Один из последних просмотренных фильмов?'
+// - 'НА сколько оцените его?'
+//
+// Ответы поместить в отдельные переменные
+//
+// Записать ответы в обьект movies в формате:
+// movies: {
+//     'logan': '8.1'
+// }
+// проверить  на ошибки в консоли
+
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', ''); // плюс перед prompt для того чтобы возвращалось число
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+const lastSeeMovie = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingFilm = prompt('НА сколько оцените его', ''),
+      lastSeeMovie2 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingFilm2 = prompt('НА сколько оцените его', '');
+// ключ - значение
+//добавление значений в обьект в виде ключ - значение
+personaMovieDB.movies[lastSeeMovie] = ratingFilm;
+personaMovieDB.movies[lastSeeMovie2] = ratingFilm2;
+
+// personaMovieDB.movies.lastSeeMovie = ratingFilm;
+// personaMovieDB.movies.lastSeeMovie2 = ratingFilm2;
+console.log(personaMovieDB);
+console.log(personaMovieDB.movies);
+
+
